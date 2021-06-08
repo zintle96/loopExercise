1d5d633b565c433be66fd20fc671e723aec42f64
Zintle Meslane <59652878+zintle96@users.noreply.github.com>
2021-06-08 10:31:40 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,64 @@
+
+/*for (var z = 0; z <= 20; z++) {
+console.log(z)
+};
+
+
+var drinks = ['coffee', 'tea', 'soda', 'water', 'tequila', 'cider', 'lemon juice']
+var y;
+for (var y = 0; y < drinks.length; y++) {
+    console.log(drinks[y])
+};
+
+x = 0
+while (x < 1000) {
+    console.log(x);
+    x++
+};
+
+a = 0
+while (a < 2000) {
+    console.log(a);
+    a = a + 2
+};
+
+cars = ['BMW', 'VW', 'TOYOTA', 'BUGATTI'];
+// console.log(cars[0]);
+cars.forEach (item => {
+    console.log(item);
+});*/
+
+
+
+// car =[{
+//     carName:[bmw,wv,],
+//     carAge :5,
+//     carColor :'blue',
+// },
+// {
+//     carName:'vw',
+//     carAge :10,
+//     carColor :'yellow',
+// },
+// {
+//     carName: 'audi',
+//     carAge: '7',
+//     carColor: 'red',
+// }
+// ]
+
+// // console.log(car[2].carName +" "+car[2].carAge + " " + car[1].carColor)
+
+// car.forEach(element => {
+//     console.log(element.carName +" "+ element.carAge + " " + element.carColor)
+// });
+
+plates = 
+
+cars = ["BMW", "Toyota",{Plates: ["CAAT", "CAF288"]}];
+cars.forEach (item => {
+console.log(item);
+    if(item.Plates !== undefined){
+        console.log(item.Plates)
+    }
+});
